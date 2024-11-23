249a2f5304a0a17b9c3ff758972b913c11e5efdb
hayk-simonyan <hayksimonyan7@gmail.com>
2024-11-23 14:25:08 +0400

    remove the host from app listen

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
